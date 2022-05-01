5e504c8f140e0562e729ca3995bd0799b73da519
enable7997 <104685110+enable7997@users.noreply.github.com>
2022-05-01 23:51:14 +0900

    Update PropertiesEx.java

--- programming/properties/PropertiesEx.java
+++ programming/properties/PropertiesEx.java
@@ -9,13 +9,13 @@ public class PropertiesExample {
 		path = URLDecoder.decode(path, "utf-8");  //path의 경로위치를 utf-8로 다시 복원
 		properties.load(new FileReader(path));    //파일 경로에 있는 properties 파일을 FileReader()메소드로 읽어들이기
 		
-		String A = properties.getProperty("A");
-		String B = properties.getProperty("B");
-		String c = properties.getProperty("C");
+		String A = properties.getProperty("A");		//A 변수에 'A'라는 키로 대응되는 값 저장
+		String B = properties.getProperty("B");		//B 변수에 'B'라는 키로 대응되는 값 저장
+		String c = properties.getProperty("C");		//C 변수에 'C'라는 키로 대응되는 값 저장
 		 
 		
-		System.out.println("A : " + A);
-		System.out.println("B : " + B);
-		System.out.println("C : " + C);
+		System.out.println("A : " + A);		//"A : apple" 출력
+		System.out.println("B : " + B);		//"B : banana" 출력
+		System.out.println("C : " + C);		//"C : candy" 출력
 	}
 }
